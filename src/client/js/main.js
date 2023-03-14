900927ef845314c1b1c976e14c3a1192c6b1d3ab
Suhyeon Lee <114565244+Lesh97@users.noreply.github.com>
2023-03-14 19:28:14 +0900

    added random circle background and change header h1 singcube neon

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,2 +1,33 @@
 import "../scss/styles.scss";
 import regeneratorRuntime from "regenerator-runtime";
+
+alert("실행");
+
+const createCircle = () => {
+  const bgWhole = document.querySelector(".bghomepage");
+  const circle = document.createElement("span");
+  const circleNames = ["circle-green", "circle-pink", "circle-blue"];
+  const circleName = circleNames[getRandomIntInclusive(0, 2)];
+  function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  circle.classList.add(circleName);
+
+  circle.style.borderRadius = 100 + "px";
+
+  circle.style.zIndex = -1;
+
+  const size = Math.random() * 50;
+  circle.style.width = 20 + size + "px";
+  circle.style.height = 20 + size + "px";
+  const { innerWidth, innerHeight } = window;
+  circle.style.left = Math.random() * (innerWidth - 100) + "px";
+  circle.style.top = Math.random() * (innerHeight - 100) + "px";
+  bgWhole.appendChild(circle);
+  setTimeout(() => {
+    circle.remove();
+  }, 5000);
+};
+
+setInterval(createCircle, 500);
