8f5445bbbcc73366b2bed2ca48ef3ac6935fe878
Suhyeon Lee <114565244+Lesh97@users.noreply.github.com>
2023-03-14 23:38:31 +0900

    totaltime updated

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -148,7 +148,9 @@ playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleInputVolumeChange);
 volumeRange.addEventListener("change", handleChangeVolumeRange);
-video.addEventListener("loadedmetadata", handleLoadedMetaData);
+video.readyState
+  ? handleLoadedMetaData
+  : video.addEventListener("loadedmetadata", handleLoadedMetaData);
 video.addEventListener("timeupdate", handleTimeUpdate);
 video.addEventListener("ended", handleEnded);
 videoContainer.addEventListener("mousemove", handleMouseMove);
